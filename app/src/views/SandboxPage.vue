c86a302908343abe35be3088a89c459188c42da2
Ben Hong <ben@bencodezen.io>
2025-06-05 10:12:00 -0500

    feat: add basetextinput scaffold (wip)

---
+++ app/src/views/SandboxPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import BaseTextInput from '@/components/BaseTextInput.vue'
+</script>
+
+<template>
+  <h1>Sandbox Page</h1>
+  <BaseTextInput placeholder="My test placeholder" />
+</template>
+
+<style scoped></style>
